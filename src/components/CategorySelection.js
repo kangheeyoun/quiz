d5646846e8e75c9a5c8b2ec6fb8be81221676b4e
kangheeyoun <kkhy0304@naver.com>
2025-02-24 17:00:13 +0900

    up

--- src/components/CategorySelection.js
+++ src/components/CategorySelection.js
@@ -3,9 +3,9 @@ import '../components/CategorySelection.css';
 const CategorySelection = ({ categories, onCategorySelect }) => {
     return (
         <div className="category-selection">
-            <h1>"우리 가족 반려견"</h1>
-            <h2>어디까지 알고 계신가요?</h2>
-            <img src="../images/송2.png" alt="반려견 이미지" />
+            <h1>"우리 가족, 반려견"</h1>            
+            <h2>어디까지 알고 계신가요?</h2>             
+            <img src={`${process.env.PUBLIC_URL}/images/hi.png`} alt="반려견 이미지" />
             <div className="ham-1"></div>
             <div className="start">
             </div>
--- src/components/Results.js
+++ src/components/Results.js
@@ -4,7 +4,8 @@ const Results = ({score,onRestart}) => {
     return (
         <div className="results">  
             <p>퀴즈종료</p>
-            <p>최종점수 : {score}점 / 30점 </p>
+            <img src={`${process.env.PUBLIC_URL}/images/bye.png`} alt="반려견 이미지" />
+            <p>{score}점 / 30점 </p>
             <button onClick={(onRestart)}>다시 시작하기</button>
         </div>
     );
